fbbe41e2da88ffe5ea9052a04d9bcdb8c60ef36b
Moukthika-br <mothukurimoukthika@gmail.com>
2025-09-08 00:11:41 +0530

    try

---
+++ catalogue.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+#check the user has root privelages or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e " $2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e " $2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf module disable nodejs-y &>>$LOG_FILE
+VALIDATE $? "Disabling the  defaultnodejs"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "enabling the nodejs"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing the nodejs"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+VALIDATE $? "adding the application user"
+
+mkdir /app &>>$LOG_FILE
+VALIDATE $? "making the application "
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip  &>>$LOG_FILE
+VALIDATE $? "Downloading the roboshop user application"
+
+cd /app
+unzip /tmp/catalogue.zip
+VALIDATE $? "unzipping the file"
+
+npm install &>>$LOG_FILE
+VALIDATE $? "Installing dependencies"
+
+cp  $SCRIP_DIR/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Copying catalogue service"
+
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
+VALIDATE $? "Starting Catalogue"
+
+cp  $SCRIP_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "Installing MongoDB client"
+
+mongosh --host mongodb.moukthika.site </app/db/master-data.js &>>$LOG_FILE
+
+
+
+
+
