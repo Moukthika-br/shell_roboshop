f5bae4ff7fb9c1c21e6c036e2d873accee5a574a
Moukthika-br <mothukurimoukthika@gmail.com>
2025-09-08 18:39:53 +0530

    trying hard

---
+++ frontend.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+#check the user has root privelages or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e " $2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e " $2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf module disable ngix -y  &>>$LOG_FILE
+VALIDATE $? "Disabling Defaulr Nginx"
+
+dnf module enable nginx:1.24 -y &>>$LOG_FILE
+VALIDATE $? "Enabling the Nginx"
+
+dnf install nginx -y &>>$LOG_FILE
+VALIDATE $? "Installing Nginx"
+
+systemctl enable nginx &>>$LOG_FILE
+systemctl start nginx  &>>$LOG_FILE
+VALIDATE $? "Starting nginx"
+
+rm -rf /usr/share/nginx/html/*  &>>$LOG_FILE
+VALIDATE $? "removing default content"
+
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
+VALIDATE $? "downloading the frontend"
+
+cd /usr/share/nginx/html &>>$LOG_FILE
+unzip /tmp/frontend.zip &>>$LOG_FILE
+VALIDATE $? "unzipping frontend"
+
+
+rm -rf /etc/nginx/nginx.conf &>>$LOG_FILE
+VALIDATE $? "Removinf default nginx conf"
+
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf &>>$LOG_FILE
+VALIDATE $? "Copying nginx.conf"
+
+systemctl restart nginx &>>$LOG_FILE
+VALIDATE $? "Restarting nginx"
+
+
--- roboshop.sh
+++ roboshop.sh
@@ -41,11 +41,13 @@ do
       --instance-ids "$INSTANCE_ID" \
       --query "Reservations[0].Instances[0].PublicIpAddress" \
       --output text)
+    RECORD_NAME="$instance.$DOMAIN_NAME"
   else
     IP=$(aws ec2 describe-instances \
       --instance-ids "$INSTANCE_ID" \
       --query "Reservations[0].Instances[0].PrivateIpAddress" \
       --output text)
+    RECORD_NAME="$DOMAIN_NAME"
   fi
 
   if [[ -z "$IP" || "$IP" == "None" ]]; then
@@ -64,7 +66,7 @@ do
       \"Changes\": [{
         \"Action\": \"UPSERT\",
         \"ResourceRecordSet\": {
-          \"Name\": \"$instance.$DOMAIN_NAME\",
+          \"Name\": \"$RECORD_NAME\",
           \"Type\": \"A\",
           \"TTL\": 120,
           \"ResourceRecords\": [{
