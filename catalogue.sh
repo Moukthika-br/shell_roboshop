01c42ab16dd0977145bde3e58859917e91f122f4
Moukthika-br <mothukurimoukthika@gmail.com>
2025-09-08 17:27:14 +0530

    trying hard

--- catalogue.sh
+++ catalogue.sh
@@ -78,10 +78,14 @@ cp  $SCRIPT_DIR/Mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB client"
 
-mongosh --host mongodb.moukthika.site </app/db/master-data.js &>>$LOG_FILE
-VALIDATE $? "Loading data in mongo"
-
-
+STATUS=$(mongosh --host mongodb.daws84s.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+if [ $STATUS -lt 0 ]
+then
+    mongosh --host mongodb.daws84s.site </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Loading data into MongoDB"
+else
+    echo -e "Data is already loaded ... $Y SKIPPING $N"
+fi
 
 
 
